eb0228ffab15e31a5efd8390b6060caa3b724943
Saurav Bhati <sauravbhati0991@gmail.com>
2024-07-12 23:35:42 +0530

    search bar updated

--- frontend/src/components/DetailsSec.jsx
+++ frontend/src/components/DetailsSec.jsx
@@ -20,6 +20,7 @@ export default function DetailsSection({ data }) {
       console.log(err);
     }
   };
+
   let GetDetails = async (apiKey) => {
     try {
       const Today = new Date(Date.now());
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,19 +1,82 @@
-import React from "react";
+"use client";
+import React, { useState, useEffect } from "react";
 import SearchIcon from "@mui/icons-material/Search";
+import slugify from "slugify";
 
 export default function SearchBar() {
+  let [apiKey, setApiKey] = useState("");
+  let [search, setSearch] = useState(null);
+
+  let GetApiKey = async () => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/movies`
+      );
+      const key = await response.json();
+      setApiKey(key.ApiKey);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  let handleSearch = async (e) => {
+    console.log(e.target.value);
+    try {
+      await fetch(
+        `https://api.themoviedb.org/3/search/multi?query=${e.target.value}&language=en-US&page=1&api_key=${apiKey}`
+      )
+        .then((res) => res.json())
+        .then((json) => setSearch(json.results));
+      console.log(search);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await GetApiKey();
+    };
+    fetchData();
+  });
+
   return (
-    <div className=" z-[2] absolute flex items-center xsm:top-12 xsm:w-[70%] xsm:mx-[15%] sm:w-auto sm:mx-[0px] sm:right-5 sm:top-5 bg-slate-300 rounded-[50px] hover:bg-slate-100 ">
-      <form action="" className="flex items-center xsm:w-full">
+    <div className=" z-[2] absolute flex flex-col  xsm:top-12 xsm:w-[70%] xsm:mx-[15%] sm:w-auto sm:mx-[0px] sm:right-5 sm:top-5 bg-slate-300 rounded-lg hover:bg-slate-100 ">
+      <form className="flex items-center relative xsm:w-full">
         <input
+          onChange={(e) => handleSearch(e)}
           type="text"
           placeholder="Search Movies,TV shows..."
-          className="bg-transparent rounded-[50px] w-[80%] transition-all ease-in-out duration-500 py-1 ml-2 px-5 outline-none ssm:hover:w-[300px] placeholder:text-black placeholder:text-opacity-40 hover:placeholder:text-opacity-60 "
+          className="bg-transparent rounded-lg w-[80%] transition-all ease-in-out duration-500 py-1 ml-2 px-5 outline-none ssm:hover:w-[300px] placeholder:text-black placeholder:text-opacity-40 hover:placeholder:text-opacity-60 "
         />
-        <button className=" mx-2 p-1 rounded-[50px] transition ease-in-out duration-300  hover:bg-slate-400 ">
-          <SearchIcon />
-        </button>
       </form>
+      {search != null ? (
+        <div className=" flex flex-col items-start px-5 ">
+          {search.map((v, i) => {
+            return (
+              <div>
+                <hr />
+                <a
+                  href={`/${
+                    v?.media_type === "movie"
+                      ? "movies/"
+                      : v?.media_type === "tv"
+                      ? "tvshows/"
+                      : "people/"
+                  }${slugify(v?.original_title ? v?.original_title : v?.name, {
+                    lower: true,
+                  })}-${v?.id}`}
+                >
+                  <p className=" cursor-pointer xsm:w-full sm:w-[228.8px] overflow-scroll no-scrollbar">
+                    {v?.original_title ? v?.original_title : v?.name} (
+                    {v?.media_type})
+                  </p>
+                </a>
+              </div>
+            );
+          })}
+        </div>
+      ) : null}
     </div>
   );
 }
