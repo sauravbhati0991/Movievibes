f3f1ea07bacdc3c1840d8bbfff284ff73f756ee0
Saurav Bhati <165806154+sauravbhati0991@users.noreply.github.com>
2025-04-22 16:12:45 +0530

    Update page.js

--- frontend/src/app/movies/[movieId]/page.js
+++ frontend/src/app/movies/[movieId]/page.js
@@ -2,24 +2,11 @@
 import { useParams } from "next/navigation";
 import MoviesDetails from "@/pages/MovieDetails";
 
-export async function generateStaticParams() {
-  try {
-    const response = await fetch('https://movievibes.onrender.com/api/movies');
-    const movies = await response.json();
-    
-    return movies.map((movie) => ({
-      movieId: `movie-${movie.id}`
-    }));
-  } catch (error) {
-    console.error('Error generating static params:', error);
-    return []; // Return empty array as fallback
-  }
-}
-
-export default function MovieDeatilsPage() {
+export default function MovieDetailsPage() {
   const params = useParams();
   const movieIdArr = params.movieId.split("-");
   const movieId = movieIdArr[movieIdArr.length - 1];
+  
   return (
     <>
       <MoviesDetails id={movieId} />
