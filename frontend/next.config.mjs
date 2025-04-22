eb2a3199338130a94eaff43f2cea603bdeb71a80
Saurav Bhati <165806154+sauravbhati0991@users.noreply.github.com>
2025-04-22 17:11:34 +0530

    Update next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,13 +1,14 @@
 // @type {import('next').NextConfig}
 const nextConfig = {
+  output: 'export', // This enables static export
   images: {
-    output: 'export',
-      domains: [
-        'image.tmdb.org',  // for TMDB images
-        'lh3.googleusercontent.com', // for Google auth profile pictures
-        'firebasestorage.googleapis.com' // for Firebase Storage images
-      ],
-    },
+    domains: [
+      'image.tmdb.org',  // for TMDB images
+      'lh3.googleusercontent.com', // for Google auth profile pictures
+      'firebasestorage.googleapis.com' // for Firebase Storage images
+    ],
+    unoptimized: true, // Required when using 'output: export'
+  },
 };
 
 export default nextConfig;
